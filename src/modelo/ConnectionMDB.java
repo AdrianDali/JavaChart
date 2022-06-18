1e23a58f23c887f915cabefd884c2af38510c92a
AdrianDali <hadriandali@gmail.com>
2022-06-17 23:25:42 -0500

    Inicion proyect

---
+++ src/modelo/ConnectionMDB.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author dali
+ */
+public class ConnectionMDB {
+    public static final String URL = "jdbc:mysql://localhost:3306/cardio_d?autoReconnet=true&useSSL=false";
+   // public static final String user = "adrian";
+    public static final String user = "root";
+    public static final String password = "1234";
+    
+    public Connection getConnection(){
+        Connection conection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conection = (Connection) DriverManager.getConnection(URL,user,password);
+           // System.out.println("se logro");
+            
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return conection;
+    }
+}
---
+++ src/modelo/Usuario.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author hermo
+ */
+public class Usuario {
+    private int id_usuario;
+    private String nombre;
+    private Date fecha_ingreso;
+
+    public Usuario(int id_usuario, String nombre, Date fecha_ingreso) {
+        this.id_usuario = id_usuario;
+        this.nombre = nombre;
+        this.fecha_ingreso = fecha_ingreso;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFecha_ingreso() {
+        return fecha_ingreso;
+    }
+
+    public void setFecha_ingreso(Date fecha_ingreso) {
+        this.fecha_ingreso = fecha_ingreso;
+    }
+    
+    
+    
+    
+    
+}
